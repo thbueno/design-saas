ae2ed9e941e0dfdad79150bb56eacbc3b2dfbe90
thbueno <thiagobueno.s@gmail.com>
2025-01-23 00:19:43 +0700

    add .gitignore, update next.config, and implement Navbar and Button components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,29 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-    variable: "--font-inter",
-    subsets: ["latin"],
-    display: "swap",
-    axes: ["opsz"],
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+  axes: ["opsz"],
 });
 
 export const metadata: Metadata = {
-    title: "Modern Design Tool Landing Page",
-    description: "Created with the help of Frontend Tribe",
+  title: "Modern Design Tool Landing Page",
+  description: "Created with the help of Frontend Tribe",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
-            >
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,10 +1,49 @@
+import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
+import Image from "next/image";
+
 const navLinks = [
-    { label: "Home", href: "#" },
-    { label: "Features", href: "#features" },
-    { label: "Integrations", href: "#integrations" },
-    { label: "FAQs", href: "#faqs" },
+  { label: "Home", href: "#" },
+  { label: "Features", href: "#features" },
+  { label: "Integrations", href: "#integrations" },
+  { label: "FAQs", href: "#faqs" },
 ];
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+  return (
+    <section className="py-4">
+      <div className="container">
+        <div className="items-center grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2">
+          <div>
+            <Image
+              src={logoImage}
+              alt="LayersLogo"
+              className="h-9 w-auto md:h-auto"
+            />
+          </div>
+          <div className=""></div>
+          <div className="flex justify-end gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu md:hidden"
+            >
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+            <Button variant="secondary">Log in</Button>
+            <Button variant="primary">Sign up</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
