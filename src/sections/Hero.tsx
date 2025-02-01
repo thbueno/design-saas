86e352382ccd03b4555a4b99fbeb83b166df5d2b
thbueno <thiagobueno.s@gmail.com>
2025-02-01 12:23:50 +0700

    add animations to Hero section with framer-motion

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,27 +1,83 @@
+"use client";
+
 import designExample1Image from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Button from "@/components/Button";
 import Pointer from "@/components/Pointer";
+import { motion, useAnimate } from "framer-motion";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [leftPointerScope, leftPointerAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+    leftPointerAnimate([
+      [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+      [
+        leftPointerScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+    ]);
+    rightPointerAnimate([
+      [
+        rightPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [rightPointerScope.current, { y: 0, x: 175 }, { duration: 0.5 }],
+      [
+        rightPointerScope.current,
+        { x: 0, y: [0, 20, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+  }, []);
   return (
     <section className="py-24 overflow-x-clip">
       <div className="container relative">
-        {/* Design examples */}
-        <div className="absolute -left-32 top-16 hidden lg:block">
+        {/* Design examples and Ponters */}
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, y: 100, x: -100 }}
+          className="absolute -left-32 top-16 hidden lg:block"
+        >
           <Image src={designExample1Image} alt="Design Example 1" />
-        </div>
-        <div className="absolute -right-64 -top-16 hidden lg:block">
-          <Image src={designExample2Image} alt="Design Example 2" />
-        </div>
-        {/* Pointers */}
-        <div className="absolute left-56 top-96 hidden lg:block">
+        </motion.div>
+        <motion.div
+          ref={leftPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="absolute left-56 top-96 hidden lg:block"
+        >
           <Pointer name="Andrea" />
-        </div>
-        <div className="absolute right-80 -top-4 hidden lg:block">
+        </motion.div>
+        <motion.div
+          ref={rightDesignScope}
+          initial={{ opacity: 0, y: 100, x: 100 }}
+          className="absolute -right-64 -top-16 hidden lg:block"
+        >
+          <Image src={designExample2Image} alt="Design Example 2" />
+        </motion.div>
+        <motion.div
+          ref={rightPointerScope}
+          initial={{ opacity: 0, y: 100, x: 275 }}
+          className="absolute right-80 -top-4 hidden lg:block"
+        >
           <Pointer name="Bryan" color="red" />
-        </div>
+        </motion.div>
 
         {/* Toggle null */}
         <div className="flex justify-center">
